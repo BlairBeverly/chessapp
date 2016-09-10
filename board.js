565276fbdc14ef6f501fce6826b6aad45963356f
Blair Beverly <blair.beverly@gmail.com>
2016-09-09 21:17:14 -0700

    skeleton app

---
+++ board.js
@@ -0,0 +1,27 @@
+
+
+
+  var game = new Chess();
+
+  // update board for castling, en passant and pawn promotion
+  var onSnapEnd = function() {
+    board.position(game.fen());
+  };
+
+  var onDrop = function(source, target) {
+    var move = game.move({from:source, to:target, promotion: 'q'})
+
+    if (move == null){
+      return 'snapback';
+    }
+  };
+
+  var cfg = {
+    draggable: true,
+    dropOffBoard: 'snapback',
+    position: 'start',
+    onDrop: onDrop,
+    onSnapEnd: onSnapEnd
+  };
+
+  var board = ChessBoard('board', cfg);
